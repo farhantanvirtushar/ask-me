6f8da04149ae8980e9172ae06cdd5ac37cb6e339
tushar <farhantanvir.2340.csedu@gmail.com>
2023-01-05 09:27:11 +0600

    Initial Commit

---
+++ src/main/java/com/farhantanvir/askme/AskMeApplication.java
@@ -0,0 +1,13 @@
+package com.farhantanvir.askme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farhantanvir/askme/AskMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farhantanvir.askme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
